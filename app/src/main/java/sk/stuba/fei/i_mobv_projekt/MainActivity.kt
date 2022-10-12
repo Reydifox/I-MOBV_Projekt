517e266a25fd75f68d60b05352c68ba096138774
Bittner, Radovan (SHS TE DC SVK D&A DEV2) <radovan.bittner@siemens-healthineers.com>
2022-10-12 12:14:51 +0200

    feat: task 2

--- app/src/main/java/sk/stuba/fei/i_mobv_projekt/MainActivity.kt
+++ app/src/main/java/sk/stuba/fei/i_mobv_projekt/MainActivity.kt
@@ -1,25 +1,15 @@
 package sk.stuba.fei.i_mobv_projekt
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import sk.stuba.fei.i_mobv_projekt.R
+import sk.stuba.fei.i_mobv_projekt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener { serveDrink() }
-    }
-
-    /**
-     * Roll the dice and update the screen with the result.
-     */
-    private fun serveDrink() {
-        val animView: View = findViewById(R.id.animation_view)
-        animView.visibility = View.VISIBLE
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
